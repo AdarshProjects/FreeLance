dc034b25c2f9ac86c24a1455fa3cc81ddc476e15
Adarsh Kumar <adarsh12042005@gmail.com>
2024-11-11 19:08:53 +0530

    Added User Panel

---
+++ frontend/src/components/User_panel.jsx
@@ -0,0 +1,125 @@
+
+export default function Userpanel(){
+    return (
+        <>
+            <div className="flex justify-between p-2">
+                <div className="min-w-32 h-10 bg-slate-300 grid place-items-center rounded">
+                    <p className="item-center ">LOGO</p>
+                </div>
+                <div className="flex w-1/2 justify-evenly mt-2 font-semibold cursor-pointer">                                   
+                    <div className="text-center">
+                        Home
+                    </div>
+                    <div className="text-center">
+                        Appointment
+                    </div>
+                    <div className="text-c
+                    enter">
+                        Features
+                    </div>
+                    <div className="text-center">
+                        Listings
+                    </div>
+                    <div className="text-center">
+                        Login/Signup
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="p-3 bg-[#C2F2F2] flex justify-around">
+                <select className="border w-64 h-9 rounded-md">
+                    <option>State</option>
+                    <option>Uttar Pradesh</option>
+                    <option>Delhi</option>
+                    <option>Raipur</option>
+                    <option>Bombay</option>
+                </select>
+                <select className="border w-64 h-9 rounded-md">
+                    <option>District</option>
+                    <option>Agra</option>
+                    <option>Lucknow</option>
+                    <option>Meerut</option>
+                    <option>Varanasi</option>
+                    <option>Lucknow</option>
+                </select>
+                <select className="border w-64 h-9 rounded-md">
+                    <option>Doctor/Speciality</option>
+                    <option>ENT</option>
+                    <option>CARDIOLOGY</option>
+                    <option>GYNOLOGY</option>
+                    <option>AYURVEDIC</option>
+                </select>
+            </div>
+
+
+            <div className="p-3 grid grid-cols-2 gap-3">
+            <div className="w-auto p-6 rounded-lg flex shadow-2xl shadow-sky-200 mt-9">
+                    <div className="w-40 h-40 bg-slate-300 rounded-full mt-6">
+                        <p className="text-7xl flex items-center justify-center translate-y-8">R</p>
+                    </div>
+                    <div className="ml-14 w-72">
+                        <p className="text-4xl font-semibold">Dr. Raghu Datta</p>
+                        <div><span className="font-semibold text-xl">Address-</span>HOUSE NO. 1111 VPOJSKNSK DISTT.  GFDGERE G.R  PIN CODE- 129493 INDIA  NEW DELHI</div>
+                        <div><span className="font-semibold text-xl">Reviews -</span>VERY GOOD APP</div>
+                        <div><span className="font-semibold text-xl">Years of Experience -</span> 5 YEARS</div>
+                        <div><span className="font-semibold text-xl">Speciality -</span> ENT , CARDIOLOGY</div>
+                        <div className="flex">
+                            <button className="p-1 border-2 bg-white w-36 rounded-md ml-6 font-medium">Appointment</button>
+                            <div className="ml-14 w-10"> Consultation Fee-$299</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-auto p-6 rounded-lg flex shadow-2xl shadow-sky-200 mt-9">
+                    <div className="w-40 h-40 bg-slate-300 rounded-full mt-6">
+                        <p className="text-7xl flex items-center justify-center translate-y-8">R</p>
+                    </div>
+                    <div className="ml-14 w-72">
+                        <p className="text-4xl font-semibold">Dr. Raghu Datta</p>
+                        <div><span className="font-semibold text-xl">Address-</span>HOUSE NO. 1111 VPOJSKNSK DISTT.  GFDGERE G.R  PIN CODE- 129493 INDIA  NEW DELHI</div>
+                        <div><span className="font-semibold text-xl">Reviews -</span>VERY GOOD APP</div>
+                        <div><span className="font-semibold text-xl">Years of Experience -</span> 5 YEARS</div>
+                        <div><span className="font-semibold text-xl">Speciality -</span> ENT , CARDIOLOGY</div>
+                        <div className="flex">
+                            <button className="p-1 border-2 bg-white w-36 rounded-md ml-6 font-medium">Appointment</button>
+                            <div className="ml-14 w-10"> Consultation Fee-$299</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-auto p-6 rounded-lg flex shadow-2xl shadow-sky-200 mt-9">
+                    <div className="w-40 h-40 bg-slate-300 rounded-full mt-6">
+                        <p className="text-7xl flex items-center justify-center translate-y-8">R</p>
+                    </div>
+                    <div className="ml-14 w-72">
+                        <p className="text-4xl font-semibold">Dr. Raghu Datta</p>
+                        <div><span className="font-semibold text-xl">Address-</span>HOUSE NO. 1111 VPOJSKNSK DISTT.  GFDGERE G.R  PIN CODE- 129493 INDIA  NEW DELHI</div>
+                        <div><span className="font-semibold text-xl">Reviews -</span>VERY GOOD APP</div>
+                        <div><span className="font-semibold text-xl">Years of Experience -</span> 5 YEARS</div>
+                        <div><span className="font-semibold text-xl">Speciality -</span> ENT , CARDIOLOGY</div>
+                        <div className="flex">
+                            <button className="p-1 border-2 bg-white w-36 rounded-md ml-6 font-medium">Appointment</button>
+                            <div className="ml-14 w-10"> Consultation Fee-$299</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-auto p-6 rounded-lg flex shadow-2xl shadow-sky-200 mt-9">
+                    <div className="w-40 h-40 bg-slate-300 rounded-full mt-6">
+                        <p className="text-7xl flex items-center justify-center translate-y-8">R</p>
+                    </div>
+                    <div className="ml-14 w-72">
+                        <p className="text-4xl font-semibold">Dr. Raghu Datta</p>
+                        <div><span className="font-semibold text-xl">Address-</span>HOUSE NO. 1111 VPOJSKNSK DISTT.  GFDGERE G.R  PIN CODE- 129493 INDIA  NEW DELHI</div>
+                        <div><span className="font-semibold text-xl">Reviews -</span>VERY GOOD APP</div>
+                        <div><span className="font-semibold text-xl">Years of Experience -</span> 5 YEARS</div>
+                        <div><span className="font-semibold text-xl">Speciality -</span> ENT , CARDIOLOGY</div>
+                        <div className="flex">
+                            <button className="p-1 border-2 bg-white w-36 rounded-md ml-6 font-medium">Appointment</button>
+                            <div className="ml-14 w-10"> Consultation Fee-$299</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
