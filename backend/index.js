a7c6613d74afe1021fc35a41a9d274179f1d1100
Adarsh Kumar <adarsh12042005@gmail.com>
2024-11-26 12:47:22 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mainrouter = require("./Routers/index");
-const port = process.env.PORT || 3000;
+const port =  3000;
 
 //Some middlware type setup
 app.use(cors());
